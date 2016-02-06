d153504c196d46a71f99cdb35d4413637815ad53
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-06 15:40:08 -0800

    Create Mammal Class

---
+++ src/main/java/com/sqa/jf/pet/vet/Mammal.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: Mammal.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Mammal //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Mammal extends Pet {
+
+	private boolean rabiesShot;
+
+	private boolean houseBroken;
+}
