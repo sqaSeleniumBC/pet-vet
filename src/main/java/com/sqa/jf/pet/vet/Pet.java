c9b5570cc895f29214fa15f4f1ecae164339fc4f
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-06 15:39:19 -0800

    Create Pet Class

---
+++ src/main/java/com/sqa/jf/pet/vet/Pet.java
@@ -0,0 +1,32 @@
+/**
+ * File Name: Pet.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Pet //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Pet {
+
+	private boolean alive;
+
+	private String name;
+
+	private boolean injured;
+
+	private boolean male;
+
+	private String breed;
+}
