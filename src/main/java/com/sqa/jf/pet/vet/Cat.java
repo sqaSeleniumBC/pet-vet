e3a21166689839e3d5f55789e92d21fa893d97cd
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-06 15:41:04 -0800

    Create Mammal Subclasses Cat and Dog

---
+++ src/main/java/com/sqa/jf/pet/vet/Cat.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: Cat.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Cat //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Cat extends Mammal {
+
+	private boolean killingMice;
+
+	private boolean claws;
+}
---
+++ src/main/java/com/sqa/jf/pet/vet/Dog.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: Dog.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Dog //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Dog extends Mammal {
+
+	private boolean performsAtShow;
+
+	private boolean wolfBread;
+
+	private String serviceType;
+}
