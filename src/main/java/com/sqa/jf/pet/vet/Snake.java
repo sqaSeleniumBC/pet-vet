340c113f090a18db00c0bfc5bb060a64fa9d6d35
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-06 15:51:28 -0800

    Create Lizzard and Snake Classes

---
+++ src/main/java/com/sqa/jf/pet/vet/Snake.java
@@ -0,0 +1,24 @@
+/**
+ * File Name: Snake.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Snake //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Snake extends Reptile {
+
+	private int shedFrequency;
+}
