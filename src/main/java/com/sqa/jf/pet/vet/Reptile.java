fc8584b9259004cc917feaf5f379da976d4758f9
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-06 15:40:32 -0800

    Create Reptile Class

---
+++ src/main/java/com/sqa/jf/pet/vet/Reptile.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: Reptile.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 6, 2016
+ */
+package com.sqa.jf.pet.vet;
+
+/**
+ * Reptile //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Reptile extends Pet {
+
+	private boolean scales;
+
+	private int heatLevel;
+
+	private boolean poisonous;
+}
